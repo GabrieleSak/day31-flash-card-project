433d6c8d10dc810081753ebd303aaadcfb4c6fd4
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-01-03 16:08:47 +0200

    Remove known words from list when user presses check button and updated data is saved to file called words_to_learn.csv

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ BACKGROUND_COLOR = "#B1DDC6"
 FONT_TITLE = ("Ariel", 40, "italic")
 FONT_WORD = ("Ariel", 60, "bold")
 
-words = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+try:
+    words = pandas.read_csv("data/words_to_learn.csv").to_dict(orient="records")
+except FileNotFoundError:
+    words = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+
 word = {}
 
 
@@ -28,6 +32,13 @@ def flip_card():
     canvas.itemconfig(word_text, fill="white")
 
 
+def known_word():
+    global words, word
+    words.remove(word)
+
+    pandas.DataFrame(words).to_csv("data/words_to_learn.csv", index=False)
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -43,7 +54,8 @@ word_text = canvas.create_text(400, 263, text="Word", fill="black", font=FONT_WO
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_button_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_img, bg=BACKGROUND_COLOR, relief="flat", command=next_card)
+right_button = Button(image=right_button_img, bg=BACKGROUND_COLOR, relief="flat",
+                      command=lambda: [known_word(), next_card()])
 right_button.grid(column=1, row=1)
 
 wrong_button_img = PhotoImage(file="images/wrong.png")
